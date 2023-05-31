46294777206d7527b41edc2c939a6fb5e4465de8
dheerajgharde-atri <gharde@atrihub.io>
2023-05-31 11:29:43 -0700

    feat(views): Writing your first Django app, part 3
    Created new views, added templates for the views
    
    [JiraNumber]

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # First way to implement
+    # template = loader.get_template('polls/index.html')
+
+    # return HttpResponse(template.render(context,request))
+
+    # Shortcut
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
+
+
+
+def results(request, question_id):
+    return HttpResponse("You are looking at results pf question {}".format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question {0}".format(question_id))
