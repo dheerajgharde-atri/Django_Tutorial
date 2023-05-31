08c74a43bf0069645fd8604756ddfdbd8478681a
dheerajgharde-atri <gharde@atrihub.io>
2023-05-31 15:42:42 -0700

    feat(views): Writing your first Django app, part 5
    Added trestcases to check the functionalities and views
    
    [JiraNumber]

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.template import loader
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 
 # Create your views here.
@@ -12,41 +13,44 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
     model = Question
-    
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'polls/detail.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#     return render(request, 'polls/index.html', context)
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'polls/results.html', context)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         'question': question
+#     }
+#     return render(request, 'polls/detail.html', context)
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         'question': question
+#     }
+#     return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
